6f1aa5c71689a5306a0b44d8881f4f542f0c0291
Diao Diallo <diao_diallo@gn.jsi.com>
2018-08-10 08:26:40 +0100

    Remove comments

--- src/Controller/UnflagCoursesController.php
+++ src/Controller/UnflagCoursesController.php
@@ -27,16 +27,12 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 class UnflagCoursesController extends ControllerBase {
 
   protected $entityTypeManager;
-
-  //ActionLinkController
-
+  
   /**
    * {@inheritdoc}
    */
   public function remove($user_id, $entity_id) {
 
-    \Drupal::logger('Unflag_coureses')->info('In the unflag controller');
-    \Drupal::logger('Unflag_coureses')->info('User id : ' .$user_id. ' Entity id : '. $entity_id);
 
     $flagId = 'save_list';
 
@@ -48,15 +44,9 @@ class UnflagCoursesController extends ControllerBase {
     //GET USER BY ID $user_id
     $user = \Drupal\user\Entity\User::load($user_id);
 
-    \Drupal::logger('Unflag_coureses')->info('The course subscriber : ' .$user->getAccountName());
-
 
     //GET ENTITY BY ID $entity_id
-    //$entity = \Drupal\Core\Entity\Entity::load($entity_id);
-    //$entity = EntityInterface::load($entity_id);
     $entity = Node::load($entity_id);
-
-    \Drupal::logger('Unflag_coureses')->info('The entity received : ' .$entity->id());
 
     //UNFLAG THE COURSE using service
     \Drupal::service('flag')->unflag($flag, $entity, $user);
@@ -65,36 +55,16 @@ class UnflagCoursesController extends ControllerBase {
 
     //REDIRECT TO COURSES @user_id
     $current_user_id = \Drupal::currentUser()->id();
-    ///node/add/training_list?destination=/user/{{ raw_arguments.uid }}%23traininglists
 
-    //$refresh = 'https://fpntcd8.lndo.site';
-
-    //$destination = Url::fromUserInput(\Drupal::destination()->get());
-    //$destination = Url::fromUserInput($refresh);
-    //$params = array('id' => $current_user_id);
-
-   // $url = new Url('https://fpntcd8.lndo.site',[$current_user_id])​;
-    //return $this->redirect($url);
-
-/*
-    if ($destination->isRouted()) {
-      // Valid internal path.
-      \Drupal::logger('Unflag_coureses')->info('Route name : ' .$destination->getRouteName());
-      return $this->redirect($destination->getRouteName());
-    }else{
-      \Drupal::logger('Unflag_coureses')->info('Not a valid route ');
-    }*/
 
     $routeName = 'entity.user.canonical';
-    $routeParameters = ['user' => $current_user_id]; // id of your user
+    $routeParameters = ['user' => $current_user_id];
     $options = [
       'fragment' => 'trainingusers',
     ];
+
     $url = \Drupal::url($routeName, $routeParameters, $options);
+
     return new RedirectResponse($url);
-
-
   }
-
-
 }
